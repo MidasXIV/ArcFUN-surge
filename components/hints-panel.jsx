cb7d9974be314c110217d200365864d3c6b97b61
MidasXIV <amanshaikh.arc@gmail.com>
2021-04-03 00:51:14 +0400

    Update Hints panel to use hints passed as Props

--- components/hints-panel.jsx
+++ components/hints-panel.jsx
@@ -1,4 +1,4 @@
-const HintsPanel = () => {
+const HintsPanel = ({ hints }) => {
   return (
     <div className="pb-3 bg-black rounded-lg w-full h-full md:w-1/3 lg:w-1/4 font-mono">
       <h2 className="p-3 text-white text-2xl font-black border-b border-gray-800">
@@ -7,17 +7,17 @@ const HintsPanel = () => {
       <div className="px-3 py-5 text-gray-300">
         {/* Hint Panel Item */}
         <h3 className="text-gray-600 font-black">Hint 1</h3>
-        Hints .... Unlocks at 4:30 PM Tuesday Unlocks at 4:30 PM Tuesday
+        {hints[0].hint}
       </div>
       <div className="px-3 py-5 text-gray-300">
         {/* Hint Panel Item */}
         <h3 className="text-gray-600 font-black">Hint 2</h3>
-        Unlocks at 2:30 PM Tuesday
+        {hints[1].hint}
       </div>
       <div className="px-3 py-5 text-gray-300">
         {/* Hint Panel Item */}
         <h3 className="text-gray-600 font-black">Hint 3</h3>
-        Unlocks at 4:30 PM Tuesday
+        {hints[2].hint}
       </div>
     </div>
   );
