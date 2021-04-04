ee7b459216cda5a7c9be9e908ab07dc0e1a38a9c
MidasXIV <amanshaikh.arc@gmail.com>
2021-04-05 01:56:28 +0400

    Make hints panel modular

--- components/hints-panel.jsx
+++ components/hints-panel.jsx
@@ -1,24 +1,23 @@
+const HintPanelItem = ({ title, body }) => (
+  <div className="px-3 py-5 text-gray-300">
+    <h3 className="text-gray-600 font-black">{title}</h3>
+    {body}
+  </div>
+);
+
 const HintsPanel = ({ hints }) => {
   return (
     <div className="pb-3 bg-black rounded-lg w-full h-full md:w-1/3 lg:w-1/4 font-mono">
       <h2 className="p-3 text-white text-2xl font-black border-b border-gray-800">
-        Hints Panel
+        Hints panel
       </h2>
-      <div className="px-3 py-5 text-gray-300">
-        {/* Hint Panel Item */}
-        <h3 className="text-gray-600 font-black">Hint 1</h3>
-        {hints?.[0]?.hint}
-      </div>
-      <div className="px-3 py-5 text-gray-300">
-        {/* Hint Panel Item */}
-        <h3 className="text-gray-600 font-black">Hint 2</h3>
-        {hints?.[1]?.hint}
-      </div>
-      <div className="px-3 py-5 text-gray-300">
-        {/* Hint Panel Item */}
-        <h3 className="text-gray-600 font-black">Hint 3</h3>
-        {hints?.[2]?.hint}
-      </div>
+      {hints?.map((hint, idx) => (
+        <HintPanelItem
+          key={hint?.id || `hint_${idx + 1}`}
+          title={`Hint ${idx + 1}`}
+          body={hint.hint}
+        />
+      ))}
     </div>
   );
 };
