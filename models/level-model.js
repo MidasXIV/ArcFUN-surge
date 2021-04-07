9cf4a21c70f0decb1efb3fb0cd42d7569743ad7c
MidasXIV <amanshaikh.arc@gmail.com>
2021-04-08 00:26:30 +0400

    Add API route to get all levels from the database - uses the createHandlers rest-utils - add bunch of level-model methods to get all levels and use query and projection objects

--- models/level-model.js
+++ models/level-model.js
@@ -6,23 +6,54 @@ import Level from "../schemas/level-schema";
 export default class LeveLModel {
   async getLevelIDs() {
     console.log(`Fetching Level IDs`);
-    const { client, db } = await connectToDatabase();
-    const isConnected = await client.isConnected();
-    console.log(`MogoDB isConnected :: ${isConnected}`);
+    const { db } = await connectToDatabase();
     const levelIDs = await db
       .collection("levels")
       .find()
       .map((x) => x._id)
       .toArray();
-      console.log(levelIDs);
+    console.log(levelIDs);
     return JSON.stringify(levelIDs);
   }
 
+  async getLevel(query = {}, projection = {}) {
+    const { db } = await connectToDatabase();
+    const levels = await Level.find(query, projection);
+    return JSON.stringify(levels);
+  }
+
   async createLevel(LevelProps) {
-    console.log(`creating new Level: ${LevelProps}`);
-    const { client, db } = await connectToDatabase();
-    const isConnected = await client.isConnected();
-    console.log(`MogoDB isConnected :: ${isConnected}`);
-    return Level.create({ ...LevelProps });
+    console.log(`creating new Level: ${JSON.stringify(LevelProps.name)}`);
+    const { client } = await connectToDatabase();
+    return Level.create({
+      ...LevelProps
+    });
+  }
+
+  processLayer(levels) {
+    const currentDateISO = new Date().toISOString();
+    const currentDate = new Date(currentDateISO);
+
+    return levels.map((level) => {
+      const levelUnlocksAt = new Date(level.unlocksAt);
+      const state = currentDate - levelUnlocksAt > 0 ? "" : "disabled";
+
+      const summary = `level ${
+        state === "disabled" ? "Unlocks" : "Unlocked"
+      } at ${currentDate - levelUnlocksAt}`;
+
+      const hintsUnlocked = level.hints.reduce((hintsUnlocked, hint) => {
+        const hintUnlocksAt = new Date(hint.unlocksAt);
+        const unlocked = currentDate - hintUnlocksAt > 0 ? 1 : 0;
+        return hintsUnlocked + unlocked;
+      }, 0);
+
+      return {
+        title: level.name,
+        summary,
+        state,
+        hintsUnlocked
+      };
+    });
   }
 }
--- pages/api/level/index.js
+++ pages/api/level/index.js
@@ -1,16 +1,47 @@
+import createHandlers from "../../../lib/rest-utils";
 import LevelModel from "../../../models/level-model";
+import { getSession } from "../../../lib/auth-cookies";
+
+const handlers = {
+  GET: async (req, res) => {
+    const levelModel = new LevelModel();
+    const { email } = await getSession(req);
+    // if no session throw unauthorised error
+    console.log(`Getting All levels for ${email}`);
+    const levelQuery = {}; // since we want all level query object is empty.
+    const levelProjection = {
+      name: true,
+      unlocksAt: true,
+      hints: {
+        unlocksAt: true
+      }
+    };
+    let levels = await JSON.parse(
+      await levelModel.getLevel(levelQuery, levelProjection)
+    );
+
+    levels = levelModel.processLayer(levels);
+    // extract just the name,unlocksAt and the hints.
+    // Should check whether a specific level or all levels
+    // Extract which levels the user has unlocked.
+
+    // const { token } = await getSession(req)
+    // const todoModel = new TodoModel(token)
+    // const todo = await todoModel.getTodo(req.query.id)
+    res.status(200).json({ levels });
+  },
+
+  POST: async (req, res) => {
+    const levelModel = new LevelModel();
+    const { email } = await getSession(req);
+    const level = await levelModel.createLevel(req.body);
+    const { _id } = level;
+    console.log(`New level created by ${email} :: ${_id}`);
+    res.status(200).json({ _id });
+  }
+};
 
 export default async function Level(req, res) {
-  try {
-    if (req.method !== "POST") return res.status(405).end();
-    const levelModel = new LevelModel();
-    const level = await levelModel.createLevel(req.body);
-    console.log(level);
-
-    res.status(200).send({
-      done: true
-    });
-  } catch (error) {
-    res.status(error.status || 500).end(error.message);
-  }
+  const handler = createHandlers(handlers);
+  return handler(req, res);
 }
