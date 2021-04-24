cae3dd988fae05e0166028bb74dedbfb98223f8d
MidasXIV <amanshaikh.arc@gmail.com>
2021-04-25 00:09:28 +0400

    Update fetcher to handle Errors and pass forward the error in API

--- hooks/fetcher.js
+++ hooks/fetcher.js
@@ -1,7 +1,18 @@
 export default function fetcher(selector) {
   return (url) => {
     return fetch(url)
-      .then((r) => r.json())
+      .then((r) => {
+        // If the status code is not in the range 200-299,
+        if (!r.ok) {
+          const error = new Error("An error occurred while fetching the data.");
+          // Attach extra info to the error object.
+          error.info = r.statusText;
+          error.status = r.status;
+          throw error;
+        }
+
+        return r.json();
+      })
       .then((data) => {
         return selector ? data[selector] : data ?? null;
       });
