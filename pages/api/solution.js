62c151d8a990116386b2bbc5ab96457d810bdfb5
MidasXIV <amanshaikh.arc@gmail.com>
2021-05-14 01:18:23 +0400

    Update Solution API endpoint - make request only POST - add check for levelId and solution to be present in req.body - add check if the level is already solved - update status code for accessing unAuthorized code - add further logic to unlock new level for user and update user stats

--- pages/api/solution.js
+++ pages/api/solution.js
@@ -2,13 +2,20 @@ import createHandlers from "../../lib/rest-utils";
 import LevelModel from "../../models/level-model";
 import UserModel from "../../models/user-model";
 import { getSession } from "../../lib/auth-cookies";
-import { getCurrentDate } from "../../lib/date-time";
+import { getCurrentDateISO } from "../../lib/date-time";
 
 const handlers = {
-  GET: async (req, res) => {
+  POST: async (req, res) => {
     const levelModel = new LevelModel();
     /** extract level Id and solution from query */
-    const { levelId, solution } = req.query;
+    const { levelId, solution } = req.body;
+
+    if (!levelId || !solution) {
+      res
+        .status(400)
+        .send(`Either levelId or solution is missing from request quesry`);
+      return;
+    }
 
     const { email } = (await getSession(req)) || {};
     // if no session throw unauthorised error
@@ -33,6 +40,16 @@ const handlers = {
     );
 
     // do undefined check, throw error
+    // Check if level is already solved
+    /** used to find when the lvel was unlocked and if the level is solved */
+    const currentLevelProfile = userModel.getLevelProfile(user, levelId);
+    if (currentLevelProfile?.solvedAt) {
+      res.status(200).json({
+        status: "completed",
+        message: `level:${levelId} is already solved at ${currentLevelProfile.solvedAt}.`
+      });
+      return;
+    }
 
     /** extract the level, unlocksAt and solution */
     const levelQuery = {
@@ -59,7 +76,7 @@ const handlers = {
     /** if level is invalid throw custom error */
     if (!isLevelValid) {
       res
-        .status(403)
+        .status(401)
         .send(`Forbidden access. Please unlock the level and try again`);
       return;
     }
@@ -69,7 +86,7 @@ const handlers = {
 
     if (!isAnswerValid) {
       res.status(200).json({
-        status: "failure",
+        status: "fail",
         message: `${solution} is not the right answer to this level.`
       });
       return;
@@ -83,17 +100,51 @@ const handlers = {
      * -> add this to statistics DS along with time unlockedAt
      */
 
-    const thisLevel = {
+    // Get all levels user has unlocked
+    const levelsUnlockedByUser = userModel.getLevelsUnlocked(user);
+    // Get all levels status
+    const levelsStatus = await levelModel.getAllLevelStatus();
+
+    const newUnlockedLevel = levelModel.unlockNewLevel(
+      levelsUnlockedByUser,
+      levelsStatus
+    );
+
+    /** case where the level is unlocked via timer */
+    // the level's unlocked At should be taken in account
+    const thisLevelProfile = {
       levelId,
-      unlockedAt: level.unlocksAt,
-      solvedAt: getCurrentDate(),
+      unlockedAt: currentLevelProfile?.unlockedAt ?? level.unlocksAt,
+      solvedAt: getCurrentDateISO(),
       hintsTaken
     };
 
+    let updatedStatistics = userModel.updateLevelProfile(
+      user.statistics,
+      thisLevelProfile
+    );
+    // will return undefined when all levels are unlocked
+    if (newUnlockedLevel) {
+      const newUnlockedLevelProfile = {
+        levelId: newUnlockedLevel,
+        unlockedAt: getCurrentDateISO()
+      };
+      updatedStatistics = userModel.updateLevelProfile(
+        updatedStatistics,
+        newUnlockedLevelProfile
+      );
+    }
+
+    userModel.updateUser(userQuery, {
+      statistics: updatedStatistics
+    });
+
     res.status(200).json({
-      ...level,
-      isLevelValid,
-      isAnswerValid
+      status: "correct",
+      message: `${solution} is the right answer to this level; A new level is unlocked for you.`,
+      ...thisLevelProfile,
+      isAnswerValid,
+      newUnlockedLevel
     });
   }
 };
