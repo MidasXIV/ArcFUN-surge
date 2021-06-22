8d7901433c19eb2a8a8766a7cc7bae8555727e4c
MidasXIV <amanshaikh.arc@gmail.com>
2021-06-23 01:09:36 +0400

    Clean up Login form & remove inline styling

--- components/form.jsx
+++ components/form.jsx
@@ -1,55 +1,25 @@
 const Form = ({ errorMessage, onSubmit }) => (
-  <form onSubmit={onSubmit}>
-    <label htmlFor="email-input">
-      <span>Email</span>
-      <input id="email-input" type="email" name="email" required />
+  <form onSubmit={onSubmit} className="my-5 px-2">
+    <label htmlFor="email-input" className="mt-5">
+      <span className="text-gray-600  text-md">Email</span>
+      <input
+        id="email-input"
+        type="email"
+        name="email"
+        className="block w-full p-2 border rounded border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-transparent "
+        required
+      />
     </label>
 
-    <div className="submit">
-      <button type="submit">Sign Up / Login</button>
+    <div className="mt-5">
+      <input
+        type="submit"
+        value="Sign up / Login with email"
+        className="py-3 bg-blue-500 text-white w-full rounded-xl hover:bg-blue-600 cursor-pointer"
+      />
     </div>
 
-    {errorMessage && <p className="error">{errorMessage}</p>}
-
-    <style jsx>{`
-      form,
-      label {
-        display: flex;
-        flex-flow: column;
-      }
-      label > span {
-        font-weight: 600;
-      }
-      input {
-        padding: 8px;
-        margin: 0.3rem 0 1rem;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-      }
-      .submit {
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-        justify-content: space-between;
-      }
-      .submit > a {
-        text-decoration: none;
-      }
-      .submit > button {
-        padding: 0.5rem 1rem;
-        cursor: pointer;
-        background: #fff;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-      }
-      .submit > button:hover {
-        border-color: #888;
-      }
-      .error {
-        color: brown;
-        margin: 1rem 0 0;
-      }
-    `}</style>
+    {errorMessage && <p className="text-red-600  text-md">{errorMessage}</p>}
   </form>
 );
 
--- pages/login.jsx
+++ pages/login.jsx
@@ -52,18 +52,42 @@ const Login = () => {
 
   return (
     <Layout title="Surge | Login">
-      <div className="login">
-        <Form errorMessage={errorMsg} onSubmit={handleSubmit} />
+      <div className="flex min-h-screen container rounded-lg">
+        <img
+          className="w-1/2 h-full bg-cover md:block hidden rounded-lg object-cover my-6"
+          alt="hero"
+          src="https://cdn.dribbble.com/users/13754/screenshots/10285382/media/d8977cc25cdbf7e3915e9ae37509b394.png?compress=1&resize=1200x900"
+        />
+
+        <div className="md:w-1/2 max-w-lg mx-auto my-5 px-4 py-5 shadow-none">
+          <div className="text-left p-0 font-sans">
+            <h1 className="text-gray-800 font-medium text-5xl">
+              Welcome to ArcFUN <span className="text-blue-500">Surge</span>
+            </h1>
+            <h3 className="p-1 text-gray-600 text-sm mt-2">
+              Pit your wits against each other and solve a series of deviously
+              difficut puzzles with clues being scattered accross the internet.
+            </h3>
+          </div>
+          <Form errorMessage={errorMsg} onSubmit={handleSubmit} />
+
+          <section className="bg-black font-sans text-xs text-gray-400 rounded-xl py-4 px-5 m-2">
+            <h6 className="text-gray-100 font-medium text-lg mb-2">
+              Cookies... Yum...
+            </h6>
+            You do not need a password to sign-up for this site.
+            <br />
+            We use cookies to keep you logged in and deliver a seamless and
+            personalized experience!
+          </section>
+
+          <a className="" href="/login" data-test="Link">
+            <span className="block  p-5 text-center text-gray-800 text-xs">
+              15+ puzzle wizards have joined us, now it&apos;s your turn.
+            </span>
+          </a>
+        </div>
       </div>
-      <style jsx>{`
-        .login {
-          max-width: 21rem;
-          margin: 0 auto;
-          padding: 1rem;
-          border: 1px solid #ccc;
-          border-radius: 4px;
-        }
-      `}</style>
     </Layout>
   );
 };
